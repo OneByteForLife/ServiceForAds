562840386a56af70a9e11895860f28332f7b936d
root <nameerrror3301@gmail.com>
2023-02-10 21:45:00 +0200

    Добавил реконект средстами рекурсии при неудачном первичном соединении

--- pkg/database/postgres.go
+++ pkg/database/postgres.go
@@ -4,19 +4,21 @@ import (
 	"ServiceForAds/config"
 	"context"
 	"fmt"
+	"time"
 
 	pgxpool "github.com/jackc/pgx/v4/pgxpool"
 	"github.com/sirupsen/logrus"
 )
 
-func ConnectToDatabase(conf *config.Config) (*pgxpool.Pool, error) {
+func ConnectToDatabase(conf *config.Config) *pgxpool.Pool {
 	url := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s&pool_max_conns=50", conf.Database.Username, conf.Database.Password, conf.Database.Host, conf.Database.Port, conf.Database.Dbname, conf.Database.Sslmode)
-
 	pool, err := pgxpool.Connect(context.Background(), url)
 	if err != nil {
-		logrus.Errorf("Err connect to database - %s", err)
-		return nil, err
+		logrus.Errorf("database connection failed: %v [starting reconnect]", err)
+		time.Sleep(time.Second * 10)
+		ConnectToDatabase(conf)
+	} else {
+		logrus.Info("Success connect to database")
 	}
-
-	return pool, nil
+	return pool
 }
